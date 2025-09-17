0f944df743c1e7f8bf7570fa8da21a0ec0b67f22
Willian Eliseu da Silva <w.eliseu@gmail.com>
2025-09-16 23:54:01 -0300

    added all the logic in the project

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+const calculator = {
+    result: 0,
+    firstNumber: '',
+    operator: '',
+    secondNumber: ''
+};
+
+const operators = ['+', '-', '*', '/'];
+const operation = document.getElementById('operation');
+const operationResult = document.getElementById('operation-result');
+
+function clearAll() {
+    calculator.result = 0;
+    calculator.firstNumber = '';
+    calculator.secondNumber = '';
+    calculator.operator = '';
+    refreshDisplay();
+}
+
+function deleteLast() {
+    if(calculator.secondNumber.length > 0){
+        calculator.secondNumber = calculator.secondNumber.slice(0, -1);    
+    }else if(calculator.operator.length > 0){
+        calculator.operator = '';
+    }else if(calculator.firstNumber.length > 0){
+        calculator.firstNumber = calculator.firstNumber.slice(0, -1);
+    }
+    refreshDisplay();
+}
+
+function addNumber(number) {
+    let numberValue = number.innerHTML;
+    if(calculator.operator.length > 0){
+        calculator.secondNumber += numberValue;
+    }else{
+        calculator.firstNumber += numberValue;
+    }
+    refreshDisplay();
+}
+
+function addOperator(operator) {
+    let operatorValue = operator.innerHTML;
+
+    if(calculator.result > 0){
+        calculator.firstNumber = calculator.result;
+        calculator.result = 0;
+        calculator.secondNumber = '';
+    }
+
+    if(operators.includes(operatorValue)){
+        calculator.operator = operatorValue;
+    }
+    refreshDisplay();
+}
+
+function refreshDisplay(){
+    let calcResult = `${calculator.result}`;
+    if(calcResult.length > 11){
+        clearAll();
+        operationResult.innerHTML = 'ERROR';
+    }else{
+        operation.innerHTML = calculator.firstNumber + calculator.operator + calculator.secondNumber;
+        operationResult.innerHTML = calculator.result;
+    }
+}
+
+function calculate(){
+    let result = 0;
+    switch(calculator.operator){
+        case '+':
+            result = parseFloat(calculator.firstNumber) + parseFloat(calculator.secondNumber);
+            break;
+        case '-':
+            result = parseFloat(calculator.firstNumber) - parseFloat(calculator.secondNumber);
+            break;
+        case '*':
+            result = parseFloat(calculator.firstNumber) * parseFloat(calculator.secondNumber);
+            break;
+        case '/':
+            result = parseFloat(calculator.firstNumber) / parseFloat(calculator.secondNumber);
+            break;
+    }
+    calculator.result = result.toFixed(4);
+    refreshDisplay();
+}
+
+window.addEventListener('DOMContentLoaded', function(){
+    refreshDisplay();
+});
